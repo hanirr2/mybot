a34f78f1dcdf8799ae702249f738081dbdbbc5d1
hanirr2 <44902020+hanirr2@users.noreply.github.com>
2019-05-10 17:55:21 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,209 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = 'هيي'
+
+client.on('message', msg => {
+  if (msg.content === '.') {
+    msg.reply('اطلق من ينقط يا لبى <3!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === السلام عليكم ') {
+    msg.reply('وعليكم السلام ورحمه الله وبركاته ولكم ');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'كف') {
+    msg.reply('احح والله ماعيدها اييي ');
+  }
+});
+
+if(msg.content.startsWith ('~server')) {
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setThumbnail(msg.guild.iconURL)
+    .setTitle(`${msg.guild.name}`)
+    .addField('**__ Server Name | اسم السيرفر__**',`[** __${msg.guild.name}__ **]`,true)
+    .addField('**__ OwnerShip | الاونر الاساسي__**',`**${msg.guild.owner}**`,true)
+    .addField('**__ Server ID | ايدي السيرفر__**',`**${msg.guild.id}**`,true)
+    .addField('**__ Members Count | عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
+    .addField('**__ Online | الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+    .addField('**__ Verification Level | مستوي الحمايه__**',`[** __${verifLevels[msg.guild.verificationLevel]}__** ]`,true)
+    .addField('**__ Region | البلد__**',`[** __${region[msg.guild.region]}__** ]`,true)
+    .addField('**__ Text Channels | رومات كتابيه__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+    .addField('**__ Voice Channels | رومات صوتيه__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+    .addField('**__ Created At | صنع في __**',msg.guild.createdAt.toLocaleString())
+    msg.channel.send({embed:embed});
+  }
+});
+
+
+
+
+client.on('message', message => {
+              if(!message.channel.guild) return;
+    if(message.content.startsWith(prefix + 'bc')) {
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+      let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "${message.guild.name}";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('```**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**```');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+ 
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+ 
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('Broadcast')
+       .addField('Server', message.guild.name)
+       .addField('Sender', message.author.username)
+       .addField('Message', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+});
+
+
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === prefix + "قل لي احبك") {
+        if(!message.channel.guild) return message.channel.send("** هذا الامر للسيرفرات فقط ⛔  **");
+        message.channel.send("** تم ارسال رسالة في الخاص 📬 ** ")
+        let embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle("اي لوف يو")
+        .addField("<3",' انا احبك ')			    				     		    
+        message.author.sendEmbed(embed);
+    }
+});
+
+
+
+client.on('message', message => {
+
+    if (message.content === هيي كم عددهم") {
+
+    let embed = new Discord.RichEmbed()
+
+.addField('Count: ' , message.guild.memberCount)
+.setColor("RANDOM")
+.setDescription(`${message.guild.name}`)
+     message.channel.sendEmbed(embed);
+} 
+
+});
+
+
+
+client.on('message', message => {
+if(message.content === prefix + 'دخن') {
+
+    message.channel.send('**BISSSSHES IM SMOKING**').then(async msg => {
+        setTimeout(() => {
+            msg.edit('🚬');
+        }, 500);
+        setTimeout(() => {
+            msg.edit('🚬 ☁ ');
+        }, 1000);
+        setTimeout(() => {
+            msg.edit('🚬 ☁☁ ');
+        }, 1500);
+        setTimeout(() => {
+            msg.edit('🚬 ☁☁☁ ');
+        }, 2000);
+        setTimeout(() => {
+            msg.edit('🚬 ☁☁');
+        }, 2500);
+        setTimeout(() => {
+            msg.edit('🚬 ☁');
+        }, 3000);
+        setTimeout(() => {
+            msg.edit('🚬 ');
+        }, 3500);
+        setTimeout(() => {
+            msg.edit(`Finished smoking`);
+        }, 4000);
+    });
+}
+});
+
+
+client.on("message", message => {
+    
+    if (!message.content.startsWith(prefix)) return;
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+        if(command === "skin") {
+                const args = message.content.split(" ").slice(1).join(" ")
+        if (!args) return message.channel.send("** Type your skin name **");
+        const image = new Discord.Attachment(`https://minotar.net/armor/body/${args}`, "skin.png");
+    message.channel.send(image)
+        }
+    });
+
+
+
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', '«welcome»');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('🎽 | name :  ',`${member}`)
+        .addField('📢 | اطلق من دخل' , `Welcome to the server, ${member}`)
+        .addField('🆔 | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
+
+
+client.on("message", message => {
+             
+          if(!message.channel.guild) return;
+   if(message.author.bot) return;
+      if(message.content === "~image"){
+          const embed = new Discord.RichEmbed()
+ 
+      .setTitle(`هذه هي صورة سيرفر ** ${message.guild.name} **`)
+  .setAuthor(message.author.username, message.guild.iconrURL)
+    .setColor(0x164fe3)
+    .setImage(message.guild.iconURL)
+    .setURL(message.guild.iconrURL)
+                    .setTimestamp()
+ 
+   message.channel.send({embed});
+      }
+  });
+
+
+client.login(process.env.BOT_TOKEN);
